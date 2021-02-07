2886d4e6aca727ae39156f744a3060c3f1491c7f
nks1ck <nks1ck@mail.ru>
2021-02-07 14:21:34 +0300

    Add try except block

--- pass_generator/main.py
+++ pass_generator/main.py
@@ -1,17 +1,25 @@
 from all_symbols import *
+
+import sys
 import random
-import re
-
-#password_length = int(input('Введите количество символов пароля: '))
-password_length = 7
-new_password = ''
 
 
-i = 0
-for symbol in printable:
-    while i < password_length:
-        symbol = random.randint(0, len(printable))
-        new_password += printable[symbol - 1]
-        i += 1
+while True:
+    try:
+        password_length = int(input('Enter the number of characters of the password (0 - To exit): '))
 
-print(new_password)
+        new_password = ''
+
+        symbols_counter = 0
+        for symbol in printable:
+            while symbols_counter < password_length:
+                symbol = random.randint(0, len(printable))
+                new_password += printable[symbol - 1]
+                symbols_counter += 1
+
+        print(new_password)
+
+        if password_length == 0:
+            break
+    except:
+        print('Error. Try it again!')
